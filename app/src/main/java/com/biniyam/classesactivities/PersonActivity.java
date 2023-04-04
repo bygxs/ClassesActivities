040b0e5fc69f9ec6c320f60fbf7026488f635828
bygxs <114972065+bygxs@users.noreply.github.com>
2023-04-04 17:37:04 +0200

    Backing up 4 of April 2023

---
+++ app/src/main/java/com/biniyam/classesactivities/PersonActivity.java
@@ -0,0 +1,17 @@
+package com.biniyam.classesactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+      //  Person = new Person("Bob", 32, "KarlStreet");
+
+    }
+}
